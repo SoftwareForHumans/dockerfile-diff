c58feb5b5ebcc58327ead93a972b21515e855a05
João Carlos Maduro <joao-carlos.14@hotmail.com>
2021-03-24 20:57:34 +0000

    Fixed dockerfile-diff

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -4,7 +4,10 @@ const dockerfileDiffCLI = async () => {
   const dockerfile1 = process.argv[2];
   const dockerfile2 = process.argv[3];
 
-  await dockerfileDiff(dockerfile1, dockerfile2);
+  const report = await dockerfileDiff(dockerfile1, dockerfile2);
+
+  console.log("Comparison report:");
+  console.log(report);
 }
 
 dockerfileDiffCLI();
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,23 @@ import { buildImage, removeImage } from './containers';
 import { dockerDiff } from './diff';
 
 export const dockerfileDiff = async (dockerfile1: string, dockerfile2: string) => {
+  console.log("Creating images from dockerfiles");
+
   const image1 = await buildImage(dockerfile1);
+  console.log(`Image with tag ${image1} created`);
   const image2 = await buildImage(dockerfile2);
+  console.log(`Image with tag ${image1} created`);
 
+  console.log("Comparing images...");
   const diff = dockerDiff(image1, image2);
+  console.log("Comparison finished");
 
-  await Promise.all([removeImage(image1), removeImage(image2)]);
+  console.log("Removing images");
+
+  removeImage(image1)
+  console.log(`Image with tag ${image1} removed`);
+  removeImage(image2)
+  console.log(`Image with tag ${image2} removed`);
 
   return diff;
 }
