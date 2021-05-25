2a89bf7b7ad6af5a5154b93d51203503ae0b3c37
João Carlos Maduro <joaocarlos.mdr@gmail.com>
2021-05-25 01:00:47 +0100

    Renamed database file name

--- src/csv.ts
+++ src/csv.ts
@@ -3,7 +3,7 @@ import ComparisonData from './lib/ComparisonData';
 
 export const printDatabase = (records: Array<ComparisonData>) => {
     const csvWriter = createObjectCsvWriter({
-        path: 'database.csv',
+        path: 'evaluations.csv',
         header: Object.keys(records[0]).map((header) => ({ id: header, title: header })),
         fieldDelimiter: ";"
     });
